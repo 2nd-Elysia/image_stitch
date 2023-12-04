09195dd6710d2642f01e2fe0dde4a0d5887e5c2b
= <807318252ljh@gmail.com>
2023-12-04 21:52:30 +0800

    代码重构

--- stitch.py
+++ stitch.py
@@ -22,21 +22,16 @@ B_name = os.path.splitext(os.path.basename(path_b))[0]
 
 # stitch the images together to create a panorama
 stitcher = Stitcher()
-(result) = stitcher.stitch([imageA, imageB], showMatches=False)
+see = True
+result,img = stitcher.stitch([imageA, imageB], showMatches=see)
 # show the images
-# cv2.imshow("Image A", imageA)
-# pillow_image = Image.fromarray(cv2.cvtColor(imageA, cv2.COLOR_BGR2RGB))
-# pillow_image.show()
-# # cv2.imshow("Image B", imageB)
-# pillow_image = Image.fromarray(cv2.cvtColor(imageB, cv2.COLOR_BGR2RGB))
-# pillow_image.show()
-# # cv2.imshow("Keypoint Matches", vis)
-# pillow_image = Image.fromarray(cv2.cvtColor(vis, cv2.COLOR_BGR2RGB))
-# pillow_image.show()
+if see:
+	pillow_image = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+	pillow_image.show()
 # cv2.imshow("Result", result)
 pillow_image = Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
 pillow_image.show()
-pillow_image.save(f"images/{A_name}_{B_name}.png")
+pillow_image.save(f"image_test/{A_name}_{B_name}.png")
 # cv2.waitKey(0)
 # USAGE
 # python stitch.py -f image_test/a.png -s image_test/b.png 
@@ -44,3 +39,4 @@ pillow_image.save(f"images/{A_name}_{B_name}.png")
 # python stitch.py -f images/878.png -s images/631_2.png
 # python stitch.py -f images/1123.png -s images/878_631_2.png
 # python stitch.py -f images/1330.png -s images/1123_878_631_2.png
+# python stitch.py -f image/1.png -s image/2.png
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-import imutils
 import cv2
 
 def crop_black_borders(image):
@@ -12,107 +11,45 @@ def crop_black_borders(image):
     return image
 
 class Stitcher:
-	def stitch(self, images,
-		showMatches=False):
-		# unpack the images, then detect keypoints and extract
-		# local invariant descriptors from them
-		(imageB, imageA) = images
-		(kpsA, featuresA) = self.detectAndDescribe(imageA)
-		(kpsB, featuresB) = self.detectAndDescribe(imageB)
+	def stitch(self, images, showMatches=False):
+		imageA, imageB = images
+		keypoints1, featuresA = self.extract_features(imageA)
+		keypoints2, featuresB = self.extract_features(imageB)
+		good_matches = self.match_features_bf(featuresA, featuresB)
+		if not good_matches:
+			print("No good matches found.")
+			return None, None
 		# match features between the two images
-		M = self.matchKeypoints(kpsA, kpsB,
-			featuresA, featuresB)
-		# if the match is None, then there aren't enough matched
-		# keypoints to create a panorama
+		M = self.estimate_transform(keypoints1, keypoints2, good_matches)
 		if M is None:
 			return None
-		# otherwise, apply a perspective warp to stitch the images
-		# together
-		# (matches, H, status) = M
-		# result = cv2.warpPerspective(imageA, M,
-		# 	(imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
-		# result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
 		# 图像拼接
-		result = cv2.warpPerspective(imageA, M, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
-		# 检查透视变换结果的尺寸是否正确
-		if result.shape[1] < imageB.shape[1]:
-			result = cv2.resize(result, (imageB.shape[1], result.shape[0]))
-		# 图像拼接
+		result = cv2.warpPerspective(imageA, M, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]+imageB.shape[0]))
 		result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
-		# check to see if the keypoint matches should be visualized
-		# if showMatches:
-		# 	vis = self.drawMatches(imageA, imageB, kpsA, kpsB, matches,
-		# 		status)
-		# 	# return a tuple of the stitched image and the
-		# 	# visualization
-		# 	return (result, vis)
-		# # return the stitched image
-		return result
+		result = crop_black_borders(result)
+		if showMatches:
+			img6 = cv2.drawMatchesKnn(imageA,keypoints1,imageB,keypoints2,good_matches,None,flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS | cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+		else :
+			img6 = None
+		return result ,img6
 
-	def detectAndDescribe(self, image):
+	def extract_features(self, image):
 		# convert the image to grayscale
 		feature_extractor = cv2.SIFT_create()
 		keypoints, descriptors = feature_extractor.detectAndCompute(image, None)
 		return keypoints, descriptors
 
-	def matchKeypoints(self, kpsA, kpsB, featuresA, featuresB,
-		):
-		# compute the raw matches and initialize the list of actual
-		# matches
-		# matcher = cv2.BFMatcher()
-		# rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
-		# matches = []
+	def match_features_bf(self,descriptors1, descriptors2):
 		bf = cv2.BFMatcher()
-		matches = bf.knnMatch(featuresA, featuresB, k=2)
+		matches = bf.knnMatch(descriptors1, descriptors2, k=2)
 		good_matches = []
 		for m, n in matches:
 			if m.distance < 0.5 * n.distance:
 				good_matches.append(m)
-	
-		src_pts = np.float32([kpsA[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-		dst_pts = np.float32([kpsB[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-		# 使用RANSAC算法估计仿射变换矩阵
+		return good_matches
+
+	def estimate_transform(self, keypoints1, keypoints2, good_matches):
+		src_pts = np.float32([keypoints1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+		dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 		M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 		return M
-		# loop over the raw matches
-		# for m in good_matches:
-		# 	# ensure the distance is within a certain ratio of each
-		# 	# other (i.e. Lowe's ratio test)
-		# 	if len(m) == 2 and m[0].distance < m[1].distance * ratio:
-		# 		matches.append((m[0].trainIdx, m[0].queryIdx))
-
-		# # computing a homography requires at least 4 matches
-		# if len(good_matches) > 2:
-		# 	# construct the two sets of points
-		# 	ptsA = np.float32([kpsA[i] for (_, i) in matches])
-		# 	ptsB = np.float32([kpsB[i] for (i, _) in matches])
-
-		# 	# compute the homography between the two sets of points
-		# 	(H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC,
-		# 		reprojThresh)
-
-		# 	# return the matches along with the homograpy matrix
-		# 	# and status of each matched point
-		# 	return (matches, H, status)
-
-		# # otherwise, no homograpy could be computed
-		# return None
-
-	def drawMatches(self, imageA, imageB, kpsA, kpsB, matches, status):
-		# initialize the output visualization image
-		(hA, wA) = imageA.shape[:2]
-		(hB, wB) = imageB.shape[:2]
-		vis = np.zeros((max(hA, hB), wA + wB, 3), dtype="uint8")
-		vis[0:hA, 0:wA] = imageA
-		vis[0:hB, wA:] = imageB
-		# loop over the matches
-		for ((trainIdx, queryIdx), s) in zip(matches, status):
-			# only process the match if the keypoint was successfully
-			# matched
-			if s == 1:
-				# draw the match
-				ptA = (int(kpsA[queryIdx][0]), int(kpsA[queryIdx][1]))
-				ptB = (int(kpsB[trainIdx][0]) + wA, int(kpsB[trainIdx][1]))
-				cv2.line(vis, ptA, ptB, (0, 255, 0), 1)
-		# return the visualization
-		return vis
